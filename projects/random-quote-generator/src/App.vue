5859be33ff55af9eb8b846184bf0875b26b5a5b4
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2026-01-03 19:53:31 +0100

    updated

--- projects/random-quote-generator/src/App.vue
+++ projects/random-quote-generator/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import RandomQuoteGenerator from './components/RandomQuoteGenerator.vue'
+import RandomQuoteGenerator from "./components/RandomQuoteGenerator.vue";
 </script>
 
 <template>
